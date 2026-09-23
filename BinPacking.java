9c7202267fb06955354b76b45c9d2bad332c426e
realpatato <jason.t.richardii@gmail.com>
2026-09-22 22:00:06 -0400

    Created basic window that will be used for visualization. Makes use of a scale factor, window title contains the unit conversion.

---
+++ BinPacking.java
@@ -0,0 +1,8 @@
+import visuals.Visuals;
+
+class BinPacking {
+    public static void main(String[] args) {
+        Visuals v = new Visuals(0.8);
+        v.visualize();
+    }
+}
---
+++ visuals/Visuals.java
@@ -0,0 +1,19 @@
+package visuals;
+
+import javax.swing.*;
+
+public class Visuals {
+    private double scaleFactor;
+    private JFrame frame;
+
+    public Visuals(double sf) {
+        scaleFactor = sf;
+        frame = new JFrame("Bin Packing Visualization (1px = " + 1 / scaleFactor + "mm)");
+        frame.setSize((int)(1000 * scaleFactor), (int)(1000 * scaleFactor));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public void visualize() {
+        frame.setVisible(true);
+    }
+}
